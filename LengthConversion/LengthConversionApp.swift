9c436ae7d2fc214481ef7489eff2741e6c75f59c
Mykhailo <dmytrivmykhailo2@gmail.com>
2023-12-30 23:02:53 +0200

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Михайло Дмитрів on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
